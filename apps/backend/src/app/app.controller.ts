e243f8696065f5f4bb1135a0fd5b165fc61cf3f0
roberto-juniorjp <juniorjp@gmail.com>
2024-08-30 11:08:05 -0300

    feat: Docker setup for the angular, nest, postgres and adminer

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,13 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 
-import { AppService } from './app.service';
-
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
   @Get()
   getData() {
-    return this.appService.getData();
+    return String(process.env.NODE_ENV);
   }
 }
