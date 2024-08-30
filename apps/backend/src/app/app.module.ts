a425005d5b227c4c83c1dd5515d4eec25c9914ac
roberto-juniorjp <juniorjp@gmail.com>
2024-08-30 17:09:21 -0300

    feat: add all modules and services

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { OrdersModule } from './orders/orders.module';
+import { OrderItemsModule } from './order-items/order-items.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, DatabaseModule],
+  imports: [
+    AuthModule,
+    DatabaseModule,
+    ProductsModule,
+    CategoriesModule,
+    OrdersModule,
+    OrderItemsModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
