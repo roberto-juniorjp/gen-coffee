9432fbc6b887897db2b27a31afd1afdea4fea28b
roberto-juniorjp <juniorjp@gmail.com>
2024-08-31 11:23:12 -0300

    Revert "feat: add security and some code cleanup"
    This reverts commit 006cda48202ae6dad6f742b1b47dcee07468b477.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,58 +1,39 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import csurf from 'csurf';
-import cookieParser from 'cookie-parser';
-import helmet from 'helmet';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+  .setTitle('Gen Coffee API')
+  .setDescription('Main API for the Gen Coffee API Project')
+  .setVersion('1.0')
+  .addTag('Products', 'Endpoints for managing products')
+  .addTag('Categories', 'Endpoints for managing categories')
+  .addTag('Users', 'Endpoints for managing users')
+  .addTag('Orders', 'Endpoints for managing orders')
+  .addTag('Order Items', 'Endpoints for managing order items')
+  .addBearerAuth()
+  .build();
   
-  // Set global prefix
-  app.setGlobalPrefix('api');
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
-  // Swagger setup
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('Gen Coffee API')
-    .setDescription('Main API for the Gen Coffee API Project')
-    .setVersion('1.0')
-    .addTag('Products', 'Endpoints for managing products')
-    .addTag('Categories', 'Endpoints for managing categories')
-    .addTag('Users', 'Endpoints for managing users')
-    .addTag('Orders', 'Endpoints for managing orders')
-    .addTag('Order Items', 'Endpoints for managing order items')
-    .addBearerAuth()
-    .build();
-    
-  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api/docs', app, swaggerDocument);
-
-  // Global pipes
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    forbidUnknownValues: true,
-  }));
-
-  // Security middlewares
-  app.use(cookieParser());
-  app.use(csurf({ cookie: true }));
-  app.use(helmet({ frameguard: { action: 'deny' } }));
-
-  // CORS configuration
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: '*',
+    origin: '*', // Ajuste conforme necessário
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
 
-  // Start the application
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/api`);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
