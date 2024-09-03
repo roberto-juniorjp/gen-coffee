87bf1810382dd3d9aeea8a805c7d4a4ebcbb497a
roberto-juniorjp <juniorjp@gmail.com>
2024-09-03 18:28:36 -0300

    feat: add fontawesome for menu

--- apps/frontend/src/app/core/components/main-menu/main-menu.component.ts
+++ apps/frontend/src/app/core/components/main-menu/main-menu.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.scss'
 })
 export class MainMenuComponent {
-
+  faBars = faBars;
 }
