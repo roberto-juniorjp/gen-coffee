4312bd2381d9b2ef80ffb84ae88e943c838693f0
roberto-juniorjp <juniorjp@gmail.com>
2024-09-03 19:02:37 -0300

    feat: add link to logo image

--- apps/frontend/src/app/core/components/main-logo/main-logo.component.ts
+++ apps/frontend/src/app/core/components/main-logo/main-logo.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-logo',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main-logo.component.html',
   styleUrl: './main-logo.component.scss'
 })
