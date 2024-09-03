8f3daff61336537f06bf711d29cd48761a5e012e
roberto-juniorjp <juniorjp@gmail.com>
2024-09-03 17:30:24 -0300

    feat: add main logo component

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./core/components/header/header.component";
 import { FooterComponent } from "./core/components/footer/footer.component";
+import { MainLogoComponent } from "./core/components/main-logo/main-logo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainLogoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
