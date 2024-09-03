e2e0965b44f9dae7284de87609d18ad706c7a8e2
roberto-juniorjp <juniorjp@gmail.com>
2024-09-03 19:05:05 -0300

    feat: add button to menu burger

--- apps/frontend/src/app/core/components/main-menu/main-menu.component.ts
+++ apps/frontend/src/app/core/components/main-menu/main-menu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.scss'
 })
