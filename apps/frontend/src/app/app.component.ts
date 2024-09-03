052bfb95b3a67304c6e5189a41b165bcaa4f695a
roberto-juniorjp <juniorjp@gmail.com>
2024-09-03 18:29:15 -0300

    chore: make header layout adjustments

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./core/components/header/header.component";
 import { FooterComponent } from "./core/components/footer/footer.component";
 import { MainLogoComponent } from "./core/components/main-logo/main-logo.component";
+import { MainMenuComponent } from "./core/components/main-menu/main-menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainLogoComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainLogoComponent, MainMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- apps/frontend/src/app/core/components/header/header.component.ts
+++ apps/frontend/src/app/core/components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from "../../../shared/components/search-bar/search-bar.component";
+import { MainLogoComponent } from "../main-logo/main-logo.component";
+import { MainMenuComponent } from '../main-menu/main-menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchBarComponent],
+  imports: [SearchBarComponent, MainLogoComponent, MainMenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
